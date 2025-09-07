76fb035f4ee0d06983a1a985bfd5577b1d75a575
qprofessionalprofile <rivers_professionalreach@pm.me>
2025-09-06 21:42:47 -0500

    feat: initial setup with HTML, Bootstrap, and base CSS

---
+++ Codewars Goal Progress/script.js
@@ -0,0 +1,10 @@
+/*
+Furture Plans
+Make a program that:
+
+- Asks me for the points that I got when i get in.
+- The program updates the information that i input to it, to the progress bar.
+- when its added, add the percentage of the point that were added to the current percentage-
+on the bar.
+- Will store the inputs adn save them to a MySQL data base by haveing python handle the logic.
+*/
